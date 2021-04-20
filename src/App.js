a151de278703b6246afff553ae07215a602541dd
dleidi <dleidi@suse.com>
2021-04-20 17:05:06 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component<> {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render () {
+    var output = [];
+    for (var i=0; i<5; i++) {
+        output.push(<div className={i%2==0 ? "black" : "white"}>{i}</div>);
+    }
+    
+    return (
+      <div className="App">
+        { output }
+      </div>
+    );
+  }
 }
 
 export default App;
